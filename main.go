fbc6872c981b9f189615ed7a16b652fe22894d7b
aidanuno <153238287+aidanuno@users.noreply.github.com>
2025-12-19 01:22:22 +0000

    chore: remove unused comment

--- main.go
+++ main.go
@@ -1,10 +1,3 @@
-// Copyright 2025 The Go MCP SDK Authors. All rights reserved.
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file.
-
-// The hello server contains a single tool that says hi to the user.
-//
-// It runs over the stdio transport.
 package main
 
 import (
